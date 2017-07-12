3538193e40848ca74b42a369b555734ec59a871e
Hack Reactor Student <student@hackreactor.com>
2017-07-12 12:16:02 -0500

    Implement the desired functionality

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+var App = () => (
+  <div>
+    <h1> GroceryList </h1>
+    <GroceryList items={['Tequila', 'Spinach', 'Vodka', 'Lime', 'Tapatio']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul> 
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      hover: false
+    };
+  }
+  
+  toggleHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+  
+  render() {
+    var style;
+    
+    if (this.state.hover) {
+      style = {fontWeight: 'bold'}
+    } else {
+      style = {fontWeight: 'normal'};
+    }
+    
+    return (
+      <li style={style} onMouseEnter={this.toggleHover.bind(this)} onMouseLeave={this.toggleHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+  
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
